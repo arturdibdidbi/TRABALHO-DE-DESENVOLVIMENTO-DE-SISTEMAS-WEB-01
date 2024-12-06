2ff7505ef95ea7e3b439504f002e40ef295f6c1f
Davi Melo <davicosta01082008@gmail.com>
2024-12-05 22:18:45 -0300

    feat: integrate with api

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const form = document.querySelector('#form')
+const input = document.querySelector('#email')
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
 
-  // API call goes here
-
-  alert('BACKEND POSITIVO E OPERANTE. SEU EMAIL FOI REGISTRADO, GUERREIRO.')
+  fetch('https://mail-oirb.onrender.com/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email: input.value })
+  })
+  .then(() => {
+    alert('BACKEND POSITIVO E OPERANTE. SEU EMAIL FOI REGISTRADO, GUERREIRO.')
+  })
+  .catch(() => {
+    alert('MEUS PÊSAMES, GUERREIRO, SEU EMAIL NÃO FOI REGISTRADO.')
+  })
 })
