8f7f2e941851aaa2e8330822f4659b37d11015ef
Davi Melo <davicosta01082008@gmail.com>
2024-12-05 21:25:02 -0300

    feat: pre-integrate with api

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  // API call goes here
+
+  alert('BACKEND POSITIVO E OPERANTE. SEU EMAIL FOI REGISTRADO, GUERREIRO.')
+})
